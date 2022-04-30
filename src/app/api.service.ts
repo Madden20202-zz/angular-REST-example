7d6cd20aa02db6e1a6530ad3eeb23e539903de0d
austin <business.mocksock@gmail.com>
2022-04-30 15:48:18 -0500

    add command to make api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+// This file was created by using the command
+// ng generate service api
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  
 
   constructor() { }
 }
